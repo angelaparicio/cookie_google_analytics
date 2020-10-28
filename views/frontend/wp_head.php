49edf5940940a00dbf6a34f1c1c985e56aac9e0f
Angel <a.l.aparicio.gomez@gmail.com>
2020-10-28 11:57:27 +0100

    Refactorizacion wp_head

--- views/frontend/wp_head.php
+++ views/frontend/wp_head.php
@@ -2,15 +2,15 @@
 	
 	function cookies_y_google_analytics_head(){
 	
-		$script = get_option('rgdp_cookies_script');
-		$cookies_url = get_option('rgdp_cookies_info_page');
+		$rgdp_cookies_script = get_option('rgdp_cookies_script');
+		$rgdp_cookies_info_page = get_option('rgdp_cookies_info_page');
 		
-		if ( strlen($script) && strlen($cookies_url) ){
+		if ( strlen($rgdp_cookies_script) && strlen($rgdp_cookies_info_page) ){
 			wp_enqueue_script('rgdp_cookies', plugin_dir_url(__FILE__).'js/rgdp_cookies.js', array('jquery'));
 			wp_enqueue_style('rgdp_cookies',  plugin_dir_url(__FILE__).'css/rgdp_cookies.css');
 			
-			echo '<script>'.str_replace("\'", "'", $script).'</script>';
-			echo '<script> var cookiesURL = "'.$cookies_url.'";</script>';
+			echo stripslashes($rgdp_cookies_script);
+			echo '<script> var cookiesURL = "'.$rgdp_cookies_info_page.'";</script>';
 		}
 		
 	}
